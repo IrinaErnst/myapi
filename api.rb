edd1169e552a003945276fbe51c21f5c04057577
Irina Kalashnikova <IrinaKalashnikova@gmail.com>
2014-11-29 20:09:07 -0500

    Modified api.rb

--- api.rb
+++ api.rb
@@ -1,20 +1,29 @@
 require 'sinatra'
 require 'json'
-
+ 
 class API < Sinatra::Base
-
-	get '/' do
-  	'<html><body><h1>Hello world!</h1></body></html>'
+ 
+  get '/' do
+    '<html><body><h1>Hello world!</h1></body></html>'
   end
-
+ 
   # Test locally w/ 'curl -i http://localhost:5000/rooms.json'
+  #                 'curl -i http://localhost:5000/rooms.json\?beacon_id=abc1234\&maj_val\=1\&min_val\=1'
   get '/rooms.json' do
     content_type :json
-    
-    {:rooms => [
-                 {:id => 1, :name => "Conference Room", :beacon_id => "abc123", :maj_val => 1, :min_val => 1},
-                 {:id => 2, :name => "Class Room", :beacon_id => "abc123", :maj_val => 1, :min_val => 2}
-                ]}.to_json
+ 
+    if params[:beacon_id] == 'abc' && params[:maj_val].to_i == 1 && params[:min_val].to_i == 1
+      room_name = "Conference Room"
+    elsif params[:beacon_id] == 'abc' && params[:maj_val].to_i == 1 && params[:min_val].to_i == 2
+      room_name = "Classroom"
+    else
+      room_name = "Unknown"
+    end
+ 
+    {:name => room_name,
+     :beacon_id => params[:beacon_id],
+     :maj_val => params[:maj_val].to_i,
+     :min_val => params[:min_val].to_i}.to_json
   end
-
+ 
 end
