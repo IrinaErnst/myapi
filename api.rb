6f863679a3a5698d1d4e402d0ea55541a267756a
Irina Kalashnikova <IrinaKalashnikova@gmail.com>
2014-11-29 20:46:17 -0500

    Modified api,rb

--- api.rb
+++ api.rb
@@ -4,23 +4,23 @@ require 'json'
 class API < Sinatra::Base
  
   get '/' do
-    '<html><body><h1>Hello world!</h1></body></html>'
+    '<html><body><h1>Place-it!</h1></body></html>'
   end
  
   # Test locally w/ 'curl -i http://localhost:5000/rooms.json'
-  #                 'curl -i http://localhost:5000/rooms.json\?beacon_id=abc1234\&maj_val\=1\&min_val\=1'
-  get '/rooms.json' do
+  #                 'curl -i http://localhost:5000/rooms.json\?beacon_id=abc\&maj_val\=1\&min_val\=1'
+  get '/beacons.json' do
     content_type :json
  
     if params[:beacon_id] == 'abc' && params[:maj_val].to_i == 1 && params[:min_val].to_i == 1
-      room_name = "Conference Room"
+      beacon_name = "Green"
     elsif params[:beacon_id] == 'abc' && params[:maj_val].to_i == 1 && params[:min_val].to_i == 2
-      room_name = "Classroom"
+      beacon_name = "Purple"
     else
-      room_name = "Unknown"
+      beacon_name = "Blue"
     end
  
-    {:name => room_name,
+    {:name => beacon_name,
      :beacon_id => params[:beacon_id],
      :maj_val => params[:maj_val].to_i,
      :min_val => params[:min_val].to_i}.to_json
